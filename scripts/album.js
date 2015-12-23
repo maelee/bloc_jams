36818e666fb2bc74bacccbbd5398a7554eb77204
Sarah <maelee3@gmail.com>
2015-12-23 09:22:41 -0700

    Debugging of album.js

--- scripts/album.js
+++ scripts/album.js
@@ -126,10 +126,10 @@ window.onload = function() {
         if (event.target.parentElement.className === 'album-view-song-item') {
                 event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
                 var songItem = getSongItem(event.target);
-+
-+               if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-+                   songItem.innerHTML = playButtonTemplate;
-+               }
+
+               if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                   songItem.innerHTML = playButtonTemplate;
+               }
          }
     });
 
