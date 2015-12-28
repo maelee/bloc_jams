42c1e1182c8293792a9f69edd34fd10fd097d0cf
Sarah <maelee3@gmail.com>
2015-12-27 18:31:23 -0700

    Assignment 33 completed

--- scripts/album.js
+++ scripts/album.js
@@ -69,12 +69,11 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 var setSong = function(songNumber) {
     if (currentSoundFile) {
-         currentSoundFile.stop();
+        currentSoundFile.stop();
      }
 
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
-};
     // #1
     currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
          // #2
@@ -196,6 +195,19 @@ var updatePlayerBarSong = function() {
     $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
+var togglePlayFromPlayerbar = function() {
+     var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+     if (currentSoundFile.isPaused()) {
+         $currentlyPlayingCell.html(pauseButtonTemplate);
+         $(this).html(playerBarPauseButton);
+         currentSoundFile.play();
+     } else if (currentSoundFile) {
+         $currentlyPlayingCell.html(playButtonTemplate);
+         $(this).html(playerBarPlayButton);
+         currentSoundFile.pause();
+     }
+ };
+ 
 // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -206,6 +218,7 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
@@ -217,4 +230,5 @@ $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerbar);
 });
